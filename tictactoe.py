161c038cd2908a17875fe4940088fdbd1b4cda48
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-11-22 12:30:04 +0100

    new commit in tic tac toe

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,7 @@
 import os, random, time
 
 DISPLAY_BOARD = ['?'] * 9
-GAME_PLAYED = [] * 9
+GAME_PLAYED = () * 9
 USER_PLAY = []
 COMPUTER_PLAY = []
 COMPUTER_PICK = ""
@@ -39,14 +39,25 @@ def check_X_Or_O():
 def computer_turn():
     comp = computer_play(COMPUTER_PICK)
     insert_in_list(comp[0], comp[1])
-
+    
+    
 def player_turn():
     player = player_play(PLAYER_PICK)
     insert_in_list(player[0], player[1])
-
-# Function to insert computer play into the display board
+    
+    
+# Function to insert choosen x or o play into the display board
 def insert_in_list(index, char):
     replace(DISPLAY_BOARD, index, char)
+
+
+def check_list(list, index, player):
+    if index in list and player == "computer":
+        print(f"{index} has been picked")
+        computer_turn()
+    if index in list and player == "user":
+        print(f"{index} has been picked")
+        player_turn()
 
 
 def menu():
@@ -70,6 +81,7 @@ if __name__=="__main__":
     check_X_Or_O() #Player chooses either X or O
     while True:
         menu()
+        print(GAME_PLAYED)
         
         
 
