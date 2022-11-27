92f12ab6e2b528d463e1cb9259fc9c8c5d907180
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-11-27 16:03:14 +0100

    check win function

--- tictactoe.py
+++ tictactoe.py
@@ -8,11 +8,6 @@ CHECK_WIN = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [
 COMPUTER_PICK = ""
 PLAYER_PICK = ""
 
-'''
-0 1 2
-3 4 5
-6 7 8
-'''
 def display_doard():
     print(f"""
 
