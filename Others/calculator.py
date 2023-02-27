d8e36d2ec5c71dea2828d8d11ff423bcd2786d34
Solomon Bestz <Kingsolodawarrior@gmail.com>
2023-02-27 12:04:06 +0100

    changes made

---
+++ Others/calculator.py
@@ -0,0 +1,25 @@
+print("I can calculate in order addition, subtraction, division and multilication")
+
+print("We are adding")
+first_num = float(input("Enter First Number: "))
+second_num = float(input("Enter Second Number: "))
+
+print(f"Answer: {first_num + second_num}")
+
+print("We are subtracting")
+first_num = float(input("Enter First Number: "))
+second_num = float(input("Enter Second Number: "))
+
+print(f"Answer: {first_num - second_num}")
+
+print("We are Division")
+first_num = float(input("Enter First Number: "))
+second_num = float(input("Enter Second Number: "))
+
+print(f"Answer: {first_num / second_num}")
+
+print("We are Multiplication")
+first_num = float(input("Enter First Number: "))
+second_num = float(input("Enter Second Number: "))
+
+print(f"Answer: {first_num * second_num}")
---
+++ Others/list.py
@@ -0,0 +1,25 @@
+my_lis = [1, 2, 3, 4, 5, 6]
+count = 0
+
+# for something in my_lis:
+#     print(something)
+
+
+# while True:
+#     print(my_lis[count])
+    # count = count + 1
+
+
+x = 6
+y = 5
+
+
+"""
+addition
+"""
+# print(x + y)
+
+"""
+Subtraction
+"""
+print(x - y)
